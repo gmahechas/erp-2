d45bf4c50dde905cf05c8c8f77f4b971ee0495f4
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-01 09:14:31 -0500

    fix: fix error handler in lambda

--- packages/common/erp-common-lambdajs/src/error-handler.lambda.ts
+++ packages/common/erp-common-lambdajs/src/error-handler.lambda.ts
@@ -4,8 +4,7 @@ import { responseLambda } from './response.lambda';
 export const errorHandlerLambda = (error: Error) => {
 	let response: IResponse;
 	if (error instanceof CustomError) {
-		const { statusCode, serializeErrors } = error;
-		response = iresponse(statusCode, null, serializeErrors());
+		response = iresponse(error.statusCode, null, error.serializeErrors());
 	} else {
 		response = iresponse(500, null, 'Something went wrong');
 	}
