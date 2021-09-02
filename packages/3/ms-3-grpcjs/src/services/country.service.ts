df6c983ef1949c2e343fc2cff1cba3601c2c1f38
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-01 20:25:47 -0500

    refactor: binding controllers in grpc app

--- packages/3/ms-3-grpcjs/src/services/country.service.ts
+++ packages/3/ms-3-grpcjs/src/services/country.service.ts
@@ -1,7 +1,29 @@
 import { countryGrpcService } from '@gmahechas/erp-common-grpcjs';
+import { createOneCountry, updateOneCountry, deleteOneCountry, searchOneCountry } from '@gmahechas/erp-common-ms-3-js'
 
 export const countryService = {
-	CreateOne: (call, callback) => {
-		callback(null, call.request);
+	CreateOne: async (call, callback) => {
+		const { id, countryName, countryCode } = call.request.entity!;
+		const entity = await createOneCountry({ countryName, countryCode });
+		callback(null, { entity });
+	},
+	UpdateOne: async (call, callback) => {
+		const { id, countryName, countryCode } = call.request.entity!;
+		const entity = await updateOneCountry({ id, countryName, countryCode });
+		callback(null, { entity });
+	},
+	DeleteOne: async (call, callback) => {
+		const { id, countryName, countryCode } = call.request.entity!;
+		const entity = await deleteOneCountry({ id });
+		callback(null, { entity });
+	},
+	SearchOne: async (call, callback) => {
+		const { id, countryName, countryCode } = call.request.entity!;
+		const entity = await searchOneCountry({ id, countryName, countryCode });
+		callback(null, { entity });
+	},
+	SearchMany: async (call, callback) => {
+		const [...entities] = call.request.entities!;
+		callback(null, { entities });
 	}
 } as countryGrpcService.CountryServiceHandlers;
--- packages/3/ms-3-grpcjs/src/services/estate.service.ts
+++ packages/3/ms-3-grpcjs/src/services/estate.service.ts
@@ -1,7 +1,29 @@
 import { estateGrpcService } from '@gmahechas/erp-common-grpcjs';
+import { createOneEstate, updateOneEstate, deleteOneEstate, searchOneEstate } from '@gmahechas/erp-common-ms-3-js'
 
 export const estateService = {
-	CreateOne: (call, callback) => {
+	CreateOne: async (call, callback) => {
+		const { id, estateName, estateCode, countryId } = call.request.entity!;
+		const entity = await createOneEstate({ estateName, estateCode, countryId });
+		callback(null, { entity });
+	},
+	UpdateOne: async (call, callback) => {
+		const { id, estateName, estateCode, countryId } = call.request.entity!;
+		const entity = await updateOneEstate({ id, estateName, estateCode, countryId });
 		callback(null, call.request);
+	},
+	DeleteOne: async (call, callback) => {
+		const { id, estateName, estateCode, countryId } = call.request.entity!;
+		const entity = await deleteOneEstate({ id });
+		callback(null, call.request);
+	},
+	SearchOne: async (call, callback) => {
+		const { id, estateName, estateCode, countryId } = call.request.entity!;
+		const entity = await searchOneEstate({ id, estateName, estateCode, countryId });
+		callback(null, call.request);
+	},
+	SearchMany: async (call, callback) => {
+		const [...entities] = call.request.entities!;
+		callback(null, { entities });
 	}
 } as estateGrpcService.EstateServiceHandlers;
