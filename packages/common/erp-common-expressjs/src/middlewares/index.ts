d395c270ca01316ef7a47b7b585e6a313d80f75a
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-02 22:04:01 -0500

    refactor: rename async handler middleware

--- packages/common/erp-common-expressjs/src/middlewares/index.ts
+++ packages/common/erp-common-expressjs/src/middlewares/index.ts
@@ -1,4 +1,4 @@
 export * from './error.middleware';
 export * from './not-found.middleware';
 export * from './validator.middleware';
-export * from './request-handler.middleware';
+export * from './async-handler.middleware';
