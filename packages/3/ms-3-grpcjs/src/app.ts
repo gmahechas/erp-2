2c25d815cdd1a62e50ba4c6b8c5df3bc0900cda2
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-02 20:58:39 -0500

    refactor: index grpc

--- packages/3/ms-3-grpcjs/src/app.ts
+++ packages/3/ms-3-grpcjs/src/app.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Server, loadPackageDefinition } from '@grpc/grpc-js';
-import * as protoLoader from '@grpc/proto-loader';
+import { loadSync } from '@grpc/proto-loader';
 import dotenv from 'dotenv';
 
 import { grpcType3 } from '@gmahechas/erp-common-grpcjs';
@@ -9,10 +9,10 @@ import { estateService } from './services/estate.service';
 
 dotenv.config();
 
-const packageDefinition = protoLoader.loadSync(path.resolve(__dirname, '../node_modules/@gmahechas/erp-common-grpcjs/src/protos/3/index.proto'));
-const proto = (loadPackageDefinition(packageDefinition) as unknown) as grpcType3.ProtoGrpcType;
-
 const app = new Server();
+const packageDefinition = loadSync(path.resolve(__dirname, '../node_modules/@gmahechas/erp-common-grpcjs/src/protos/3/index.proto'));
+const proto = (loadPackageDefinition(packageDefinition) as unknown) as grpcType3.ProtoGrpcType;
 app.addService(proto.country.CountryService.service, countryService);
 app.addService(proto.estate.EstateService.service, estateService);
+
 export default app;
