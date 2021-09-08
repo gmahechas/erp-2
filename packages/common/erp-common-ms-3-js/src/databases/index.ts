7f9c967e4a6638b45b2a40cda9ec97a7ae4ce6e0
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-08 16:42:29 -0500

    feat: reuse mongo connection

--- packages/common/erp-common-ms-3-js/src/databases/index.ts
+++ packages/common/erp-common-ms-3-js/src/databases/index.ts
@@ -1,17 +1,23 @@
-import { mongodbConnect, mongodbCreateConnection, ConnectDbError, IConnectDatabases, IMongodbConnectArgs } from '@gmahechas/erp-common-ms-utils-js';
+import { mongodbConnect, mongodbCreateConnection, ConnectDbError, IConnectDatabases, IMongodbConnectArgs, mongoose } from '@gmahechas/erp-common-ms-utils-js';
 import { registerMongoModels } from '../databases/register-mongo-models';
 
 export const connectDatabases: IConnectDatabases = async (mongodbConnectArgs) => {
 	await msMongodb(mongodbConnectArgs);
 }
 
+let connection: mongoose.Mongoose;
+
 const msMongodb = async (mongodbConnectArgs: IMongodbConnectArgs) => {
 	const { uri, connectOptions } = mongodbConnectArgs;
 	if (!uri) {
 		throw new ConnectDbError();
 	}
-	await mongodbConnect(uri, connectOptions);
-	/* const connection = await mongodbCreateConnection(uri, connectOptions);
+	if (connection == undefined) {
+		console.log('new mongodb connection');
+		connection = await mongodbConnect(uri, connectOptions);
+	}
+
+/* 	const connection = await mongodbCreateConnection(uri, connectOptions);
 	for (const register of registerMongoModels) {
 		register(connection)
 	}; */
