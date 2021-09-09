752a3effe25e9c3b3ae4f3f5322d22b379ddf26e
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-08 23:45:23 -0500

    feat: add querydb

--- packages/3/ms-3-lambdajs/src/config/env.ts
+++ packages/3/ms-3-lambdajs/src/config/env.ts
@@ -1,4 +1,5 @@
 export default ({
 	msPort: process.env.MS_PORT,
-	msMongodbUri: process.env.MS_MONGODB_URI
+	msMongodbUri: process.env.MS_MONGODB_URI,
+	msQueryMongodbUri: process.env.MS_QUERY_MONGODB_URI
 });
