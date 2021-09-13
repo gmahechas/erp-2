083130f37207159bd587eb799401cfcc5c041550
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-09-13 16:14:06 -0500

    refactor: rename folder to "ms"

--- packages/common/erp-common-grpcjs/src/index.ts
+++ packages/common/erp-common-grpcjs/src/index.ts
@@ -1,27 +1,27 @@
 export * from './grpc';
 export * from './utils';
 
-export * as grpcType1 from './modules/1';
-export * as authGrpcService from './modules/1/auth/AuthService';
-export * as userGrpcService from './modules/1/user/UserService';
+export * as grpcType1 from './ms/1';
+export * as authGrpcService from './ms/1/auth/AuthService';
+export * as userGrpcService from './ms/1/user/UserService';
 
-export * as grpcType3 from './modules/3';
-export * as addressGrpcService from './modules/3/address/AddressService';
-export * as cityGrpcService from './modules/3/city/CityService';
-export * as countryGrpcService from './modules/3/country/CountryService';
-export * as estateGrpcService from './modules/3/estate/EstateService';
+export * as grpcType3 from './ms/3';
+export * as addressGrpcService from './ms/3/address/AddressService';
+export * as cityGrpcService from './ms/3/city/CityService';
+export * as countryGrpcService from './ms/3/country/CountryService';
+export * as estateGrpcService from './ms/3/estate/EstateService';
 
-export * as grpcType4 from './modules/4';
-export * as companyGrpcService from './modules/4/company/CompanyService';
-export * as officeGrpcService from './modules/4/office/OfficeService';
+export * as grpcType4 from './ms/4';
+export * as companyGrpcService from './ms/4/company/CompanyService';
+export * as officeGrpcService from './ms/4/office/OfficeService';
 
-export * as grpcType5 from './modules/5';
-export * as personGrpcService from './modules/5/person/PersonService';
-export * as typeIdentificationGrpcService from './modules/5/type_identification/TypeIdentificationService';
-export * as typePesonGrpcService from './modules/5/type_person/TypePersonService';
+export * as grpcType5 from './ms/5';
+export * as personGrpcService from './ms/5/person/PersonService';
+export * as typeIdentificationGrpcService from './ms/5/type_identification/TypeIdentificationService';
+export * as typePesonGrpcService from './ms/5/type_person/TypePersonService';
 
-export * as grpcType6 from './modules/6';
-export * as menuGrpcService from './modules/6/menu/MenuService';
-export * as profileGrpcService from './modules/6/profile/ProfileService';
-export * as profileMenuGrpcService from './modules/6/profile_menu/ProfileMenuService';
-export * as userProfileGrpcService from './modules/6/user_profile/UserProfileService';
+export * as grpcType6 from './ms/6';
+export * as menuGrpcService from './ms/6/menu/MenuService';
+export * as profileGrpcService from './ms/6/profile/ProfileService';
+export * as profileMenuGrpcService from './ms/6/profile_menu/ProfileMenuService';
+export * as userProfileGrpcService from './ms/6/user_profile/UserProfileService';
