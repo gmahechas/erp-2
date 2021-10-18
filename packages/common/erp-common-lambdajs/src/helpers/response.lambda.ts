a5c501512fa37d421a198120cb7e0f8065f5c57b
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-10-18 13:52:41 -0500

    feat: return headers in lambda

--- packages/common/erp-common-lambdajs/src/helpers/response.lambda.ts
+++ packages/common/erp-common-lambdajs/src/helpers/response.lambda.ts
@@ -2,5 +2,12 @@ import { IResponse } from '@gmahechas/erp-common';
 
 export const responseLambda = (response: IResponse) => {
 	const { statusCode, body } = response;
-	return { statusCode, body: JSON.stringify(body) };
+	return {
+		statusCode,
+		headers: {
+			"Access-Control-Allow-Headers": "*",
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
+		},
+		body: JSON.stringify(body) };
 }
