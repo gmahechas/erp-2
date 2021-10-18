9723ddfc9c34fdd7f426292958a9f22d33d284ca
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-10-18 14:14:35 -0500

    refactor: remove headers from aws api gw

--- packages/common/erp-common-lambdajs/src/helpers/response.lambda.ts
+++ packages/common/erp-common-lambdajs/src/helpers/response.lambda.ts
@@ -2,12 +2,5 @@ import { IResponse } from '@gmahechas/erp-common';
 
 export const responseLambda = (response: IResponse) => {
 	const { statusCode, body } = response;
-	return {
-		statusCode,
-		headers: {
-			"Access-Control-Allow-Headers": "*",
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
-		},
-		body: JSON.stringify(body) };
+	return { statusCode, body: JSON.stringify(body) };
 }
