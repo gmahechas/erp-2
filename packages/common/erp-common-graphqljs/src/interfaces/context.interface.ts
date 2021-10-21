ff8b97f39c39a1b8b1d06bc00a37a2ba8b3f7696
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-10-20 22:38:34 -0500

    refactor: remove auth from authMiddleware

--- packages/common/erp-common-graphqljs/src/interfaces/context.interface.ts
+++ packages/common/erp-common-graphqljs/src/interfaces/context.interface.ts
@@ -1,8 +1,6 @@
 import express from 'express';
-import { IAuth } from '@gmahechas/erp-common';
 
 export interface IContext {
 	req: express.Request;
 	res: express.Response;
-	auth: IAuth
 }
--- packages/common/erp-common-graphqljs/src/middlewares/auth.middleware.ts
+++ packages/common/erp-common-graphqljs/src/middlewares/auth.middleware.ts
@@ -2,5 +2,5 @@ import express from 'express';
 import { IContext } from '../interfaces/context.interface';
 
 export const authMiddleware = async (req: express.Request, res: express.Response): Promise<IContext> => {
-	return { req, res, auth: { id: 'theId', userName: 'theUserName', token: 'theToken' } }
+	return { req, res }
 }
