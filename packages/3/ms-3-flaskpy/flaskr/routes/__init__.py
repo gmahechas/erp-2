d25ee92bc958d8dc0f6a3f16b272c5007fc20ab4
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-02-16 10:42:05 -0500

    refactor: remove v2 routes

--- packages/3/ms-3-flaskpy/flaskr/routes/__init__.py
+++ packages/3/ms-3-flaskpy/flaskr/routes/__init__.py
@@ -1,6 +1,6 @@
 from flask import current_app, Blueprint
 from .v1 import v1_blueprint, init_routes_v1
-from .v2 import v2_blueprint, init_routes_v2
+# from .v2 import v2_blueprint, init_routes_v2
 
 main_blueprint = Blueprint('rest', __name__, url_prefix='/rest')
 
@@ -8,6 +8,6 @@ main_blueprint = Blueprint('rest', __name__, url_prefix='/rest')
 def init_routes():
     main_blueprint.register_blueprint(v1_blueprint)
     init_routes_v1()
-    main_blueprint.register_blueprint(v2_blueprint)
-    init_routes_v2()
+    # main_blueprint.register_blueprint(v2_blueprint)
+    # init_routes_v2()
     current_app.register_blueprint(main_blueprint)
