8e70d18d3de53ac23c2981498b503226cee7b795
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-02-16 09:41:36 -0500

    fix: verbs

--- packages/3/ms-3-flaskpy/flaskr/routes/v1/country.py
+++ packages/3/ms-3-flaskpy/flaskr/routes/v1/country.py
@@ -3,22 +3,22 @@ from flask import Blueprint, jsonify
 country_blueprint = Blueprint('country', __name__, url_prefix='/country')
 
 
-@country_blueprint.route('/create/one', methods=['GET'])
+@country_blueprint.route('/create/one', methods=['POST'])
 def create_one():
     return 'create/one'
 
 
-@country_blueprint.route('/update/one', methods=['GET'])
+@country_blueprint.route('/update/one', methods=['PUT'])
 def update_one():
     return 'update/one'
 
 
-@country_blueprint.route('/delete/one', methods=['GET'])
+@country_blueprint.route('/delete/one', methods=['DELETE'])
 def delete_one():
     return 'delete/one'
 
 
-@country_blueprint.route('/search/one', methods=['GET'])
+@country_blueprint.route('/search/one', methods=['POST'])
 def search_one():
     arr = [
         {"username": "gustavo", "theme": "dark", "image": "image_url"},
@@ -27,6 +27,6 @@ def search_one():
     return jsonify(arr)
 
 
-@country_blueprint.route('/search/many', methods=['GET'])
+@country_blueprint.route('/search/many', methods=['POST'])
 def search_many():
     return 'search/many'
--- packages/3/ms-3-flaskpy/flaskr/routes/v1/estate.py
+++ packages/3/ms-3-flaskpy/flaskr/routes/v1/estate.py
@@ -3,26 +3,26 @@ from flask import Blueprint
 estate_blueprint = Blueprint('estate', __name__, url_prefix='/estate')
 
 
-@estate_blueprint.route('/create/one', methods=['GET'])
+@estate_blueprint.route('/create/one', methods=['POST'])
 def create_one():
     return 'create/one'
 
 
-@estate_blueprint.route('/update/one', methods=['GET'])
+@estate_blueprint.route('/update/one', methods=['PUT'])
 def update_one():
     return 'update/one'
 
 
-@estate_blueprint.route('/delete/one', methods=['GET'])
+@estate_blueprint.route('/delete/one', methods=['DELETE'])
 def delete_one():
     return 'delete/one'
 
 
-@estate_blueprint.route('/search/one', methods=['GET'])
+@estate_blueprint.route('/search/one', methods=['POST'])
 def search_one():
     return 'search/one'
 
 
-@estate_blueprint.route('/search/many', methods=['GET'])
+@estate_blueprint.route('/search/many', methods=['POST'])
 def search_many():
     return 'search/many'
