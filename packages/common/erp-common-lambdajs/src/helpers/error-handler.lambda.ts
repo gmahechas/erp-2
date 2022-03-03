1982824ff9effde55be63e9f1bad80061e73e88c
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-03-02 21:11:29 -0500

    fix: mongoose error type

--- packages/common/erp-common-lambdajs/src/helpers/error-handler.lambda.ts
+++ packages/common/erp-common-lambdajs/src/helpers/error-handler.lambda.ts
@@ -6,7 +6,7 @@ export const errorHandlerLambda = (error: any | Error) => {
 	let response: IResponse;
 	if (error instanceof CustomError) {
 		response = iresponse(error.statusCode, null, error.serializeErrors());
-	} else if (error instanceof mongoose.Error) {
+	} else if (error instanceof mongoose.MongooseError) {
 		response = iresponse(500, null, { type: 'database: Something went wrong :(' });
 	} else {
 		response = iresponse(500, null, { type: 'unknown: Something went wrong :(' });
