8d3b8e24083c785633b26b06abd4d1b6158b4b22
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-03-03 14:42:54 -0500

    feat: fix token

--- packages/0/ms-0-graphqljs/src/middlewares/auth.middleware.ts
+++ packages/0/ms-0-graphqljs/src/middlewares/auth.middleware.ts
@@ -1,11 +1,6 @@
 import { IAuth } from '@gmahechas/erp-common';
 import { AuthError, jwtDecode } from '@gmahechas/erp-common-ms-utils-js';
 import { express, IContext } from '@gmahechas/erp-common-graphqljs';
-declare module 'express-session' {
-	interface SessionData {
-		token?: string;
-	}
-}
 
 export const authMiddleware = async (req: express.Request, res: express.Response): Promise<IContext> => {
 	const token = req.session.token;
--- packages/common/erp-common-ms-utils-js/src/helpers/express-session.ts
+++ packages/common/erp-common-ms-utils-js/src/helpers/express-session.ts
@@ -2,6 +2,12 @@ import connectRedis from 'connect-redis';
 import expressSession from 'express-session';
 import { connectToRedis } from '../databases/redis/connect.redis';
 
+declare module 'express-session' {
+	interface SessionData {
+		token?: string;
+	}
+}
+
 export const session = async (url: string, sessionOptions: expressSession.SessionOptions) => {
 	const RedisStore = connectRedis(expressSession);
 	const client = await connectToRedis(url, true);
