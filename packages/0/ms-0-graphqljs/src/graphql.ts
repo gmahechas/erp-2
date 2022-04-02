fb5910889d0da6f8d24dd279639b3b1285e15acf
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-04-02 08:30:34 -0500

    refactor: renam auth by authentication

--- packages/0/ms-0-graphqljs/src/graphql.ts
+++ packages/0/ms-0-graphqljs/src/graphql.ts
@@ -1,13 +1,13 @@
 import { ApolloServer } from '@gmahechas/erp-common-graphqljs';
 import { v1TypeDefs, v1Resolvers } from './ms';
-import { authMiddleware, errorMiddleware } from './middlewares';
+import { authenticationMiddleware, errorMiddleware } from './middlewares';
 
 export const graphqlV1 = async () => {
 	const graphql = new ApolloServer({
 		typeDefs: v1TypeDefs,
 		resolvers: v1Resolvers,
 		debug: false,
-		context: async ({ req, res }) => await authMiddleware(req, res),
+		context: async ({ req, res }) => await authenticationMiddleware(req, res),
 		formatError: (error) => errorMiddleware(error)
 	});
 	await graphql.start();
--- packages/0/ms-0-graphqljs/src/middlewares/authentication.middleware.ts
+++ packages/0/ms-0-graphqljs/src/middlewares/authentication.middleware.ts
@@ -2,7 +2,7 @@ import { IAuth } from '@gmahechas/erp-common';
 import { AuthError, jwtDecode } from '@gmahechas/erp-common-ms-utils-js';
 import { express, IContext } from '@gmahechas/erp-common-graphqljs';
 
-export const authMiddleware = async (req: express.Request, res: express.Response): Promise<IContext> => {
+export const authenticationMiddleware = async (req: express.Request, res: express.Response): Promise<IContext> => {
 	const token = req.session.token;
 	if (!req.body.query.includes('query signinAuth($data: SigninAuth)')) {
 		if (!token) {
