53c7e3c2cf98d9ed130ce74c802b62ee5df6e83d
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-04-06 08:04:36 -0500

    refactor: remove cors from graphql

--- packages/0/ms-0-graphqljs/src/app.ts
+++ packages/0/ms-0-graphqljs/src/app.ts
@@ -1,4 +1,4 @@
-import { helmet, cors } from '@gmahechas/erp-common-ms-utils-js';
+import { helmet } from '@gmahechas/erp-common-ms-utils-js';
 import { express } from '@gmahechas/erp-common-graphqljs';
 import { initSession, notFoundMiddleware } from './middlewares';
 import { graphqlV1 } from './graphql';
@@ -6,7 +6,6 @@ import { graphqlV1 } from './graphql';
 export const bootstrap = async () => {
 	const app = express();
 	app.use(helmet());
-	app.use(cors());
 	app.use(await initSession());
 	const serverV1 = await graphqlV1();
 	serverV1.applyMiddleware({
