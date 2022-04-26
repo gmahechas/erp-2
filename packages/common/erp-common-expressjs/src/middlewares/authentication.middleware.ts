da3a2900a01e9ef1b2086e2b8cb5f41d5001c807
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-04-25 21:35:08 -0500

    fix: authenticaion middleware

--- packages/common/erp-common-expressjs/src/middlewares/authentication.middleware.ts
+++ packages/common/erp-common-expressjs/src/middlewares/authentication.middleware.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { Context, jwtDecode, sendError, TypeErrorMessage } from '@gmahechas/erp-common-ms-utils-js';
+import { IAuth } from '@gmahechas/erp-common';
 
 export const authenticationMiddleware = async (request: Request, response: Response, next: NextFunction) => {
 	const { authorization: token } = request.headers;
 	if (!token) {
 		sendError(TypeErrorMessage.AUTHENTICATION);
 	}
-	const auth = jwtDecode(token);
+	const auth = jwtDecode(token) as IAuth;
 	Context.set('token', token);
 	Context.set('auth', auth);
 	next();
--- packages/common/erp-common-lambdajs/src/middlewares/authentication.middleware.ts
+++ packages/common/erp-common-lambdajs/src/middlewares/authentication.middleware.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { Context, jwtDecode, sendError, TypeErrorMessage } from '@gmahechas/erp-common-ms-utils-js';
+import { IAuth } from '@gmahechas/erp-common';
 
 export const authenticationMiddleware = (event: APIGatewayProxyEvent) => {
 	if (!event.path.includes('auth/signin')) {
@@ -7,7 +8,7 @@ export const authenticationMiddleware = (event: APIGatewayProxyEvent) => {
 		if (!token) {
 			sendError(TypeErrorMessage.AUTHENTICATION);
 		}
-		const auth = jwtDecode(token);
+		const auth = jwtDecode(token) as IAuth;
 		Context.set('token', token);
 		Context.set('auth', auth);
 	}
