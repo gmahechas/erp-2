7755f05ac35ac58c84977798622f3e122639c0bb
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-04-27 21:02:08 -0500

    feat: authorization middleware in lambda

---
+++ packages/common/erp-common-lambdajs/src/middlewares/authorization.middleware.ts
@@ -0,0 +1,21 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { Context, sendError, TypeErrorMessage } from '@gmahechas/erp-common-ms-utils-js';
+
+export const authorizationMiddleware = async (scopes: string[], event: APIGatewayProxyEvent) => {
+	const auth = Context.get('auth');
+	if (!auth) {
+		sendError(TypeErrorMessage.AUTHORIZATION);
+	}
+	const scope = JSON.parse(auth.scope);
+	for (const capability of scopes) {
+		const [service, actions] = capability.split(':');
+		if (!scope[service]) {
+			sendError(TypeErrorMessage.AUTHORIZATION);
+		}
+		for (const action of actions.split(',')) {
+			if (!scope[service].includes(action)) {
+				sendError(TypeErrorMessage.AUTHORIZATION);
+			}
+		}
+	}
+};
