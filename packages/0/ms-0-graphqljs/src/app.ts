f674e4752debc50ec31682f8662331f00295b48c
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-05-29 15:40:05 -0500

    feat: requestId middleware

--- packages/0/ms-0-graphqljs/src/app.ts
+++ packages/0/ms-0-graphqljs/src/app.ts
@@ -1,5 +1,5 @@
 import { helmet } from '@gmahechas/erp-common-ms-utils-js';
-import { express, notFoundMiddleware } from '@gmahechas/erp-common-graphqljs';
+import { express, requestIdMiddleware, notFoundMiddleware } from '@gmahechas/erp-common-graphqljs';
 import { initSession } from '@gmahechas/erp-common-ms-0-js';
 import { graphqlV1 } from './graphql';
 
@@ -7,6 +7,7 @@ export const bootstrap = async () => {
 	const app = express();
 	app.disable('x-powered-by');
 	app.use(helmet());
+	app.use(requestIdMiddleware);
 	app.use(await initSession());
 	const serverV1 = await graphqlV1();
 	serverV1.applyMiddleware({
