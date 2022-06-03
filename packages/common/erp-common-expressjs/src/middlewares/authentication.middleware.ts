ff0bda1a43c769c03a443b6bffee43af12fd7588
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-06-02 22:16:57 -0500

    feat: requestId express ms

--- packages/common/erp-common-expressjs/src/middlewares/authentication.middleware.ts
+++ packages/common/erp-common-expressjs/src/middlewares/authentication.middleware.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { IAuth } from '@gmahechas/erp-common';
-import { Context, jwtDecode, sendError, TypeErrorMessage, Winston } from '@gmahechas/erp-common-ms-utils-js';
+import { Context, jwtDecode, sendError, TypeErrorMessage, uuidv4, Winston } from '@gmahechas/erp-common-ms-utils-js';
 
 export const authenticationMiddleware = async (request: Request, response: Response, next: NextFunction) => {
+	const requestId = request.headers['x-request-id'] as string || uuidv4();
+	Context.set('requestId', requestId);
 	const { authorization: token } = request.headers;
 	const { body, originalUrl, method, params, query } = request;
 	if (!token) {
-		Winston.logger.error(TypeErrorMessage.AUTHENTICATION, { auth: JSON.stringify(null), action: originalUrl, method, payload: JSON.stringify({ body, params, query }) });
+		Winston.logger.error(TypeErrorMessage.AUTHENTICATION, { requestId: Context.get('requestId'), auth: JSON.stringify(null), action: originalUrl, method, payload: JSON.stringify({ body, params, query }) });
 		Context.clear();
 		sendError(TypeErrorMessage.AUTHENTICATION);
 	}
--- packages/common/erp-common-expressjs/src/middlewares/authorization.middleware.ts
+++ packages/common/erp-common-expressjs/src/middlewares/authorization.middleware.ts
@@ -4,20 +4,16 @@ import { Context, sendError, TypeErrorMessage, Winston } from '@gmahechas/erp-co
 export const authorizationMiddleware = (scopes: string[]) => async (request: Request, response: Response, next: NextFunction) => {
 	const auth = Context.get('auth');
 	const { body, originalUrl, method, params, query } = request;
-	if (!auth) {
-		Winston.logger.error(TypeErrorMessage.AUTHORIZATION, { auth: JSON.stringify(auth), action: originalUrl, method, payload: JSON.stringify({ body, params, query }) });
-		sendError(TypeErrorMessage.AUTHORIZATION);
-	}
 	const scope = JSON.parse(auth.scope);
 	for (const capability of scopes) {
 		const [service, actions] = capability.split(':');
 		if (!scope[service]) {
-			Winston.logger.error(TypeErrorMessage.AUTHORIZATION, { auth: JSON.stringify(auth), action: originalUrl, method, payload: JSON.stringify({ body, params, query }) });
+			Winston.logger.error(TypeErrorMessage.AUTHORIZATION, { requestId: Context.get('requestId'), auth: JSON.stringify(auth), action: originalUrl, method, payload: JSON.stringify({ body, params, query }) });
 			sendError(TypeErrorMessage.AUTHORIZATION);
 		}
 		for (const action of actions.split(',')) {
 			if (!scope[service].includes(action)) {
-				Winston.logger.error(TypeErrorMessage.AUTHORIZATION, { auth: JSON.stringify(auth), action: originalUrl, method, payload: JSON.stringify({ body, params, query }) });
+				Winston.logger.error(TypeErrorMessage.AUTHORIZATION, { requestId: Context.get('requestId'), auth: JSON.stringify(auth), action: originalUrl, method, payload: JSON.stringify({ body, params, query }) });
 				sendError(TypeErrorMessage.AUTHORIZATION);
 			}
 		}
