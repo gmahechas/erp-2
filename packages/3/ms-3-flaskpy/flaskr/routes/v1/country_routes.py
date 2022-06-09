1f6f5b52693c1959b0b4cba25a16a8002ef9b354
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-06-08 22:07:08 -0500

    feat: country controller py

--- packages/3/ms-3-flaskpy/flaskr/routes/v1/country_routes.py
+++ packages/3/ms-3-flaskpy/flaskr/routes/v1/country_routes.py
@@ -1,10 +1,12 @@
 from gmahechas_erp_common_flaskpy import Blueprint
+from gmahechas_erp_common_ms_3_py import CountryController
 
 country_blueprint = Blueprint('country', __name__, url_prefix='/country')
 
 
 @country_blueprint.route('/create/one', methods=['POST'])
 def create_one():
+    country = CountryController()
     return 'create/one'
 
 
---
+++ packages/common/erp-common-ms-3-py/gmahechas_erp_common_ms_3_py/modules/v1/country/country_controller.py
@@ -0,0 +1,19 @@
+class CountryController:
+
+    def __init__(self):
+        pass
+
+    def create_one_country(self, data):
+        return data
+
+    def update_one_country(self, data):
+        return data
+
+    def delete_one_country(self, data):
+        return data
+
+    def search_one_country(self, data):
+        return data
+
+    def search_many_country(self, data):
+        return data
