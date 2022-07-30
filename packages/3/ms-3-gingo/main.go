594a274b9e4a520d552a789dd1974bb9255060b8
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-07-30 11:16:38 -0500

    refactor: remove go projects

--- packages/3/ms-3-gingo/main.go
+++ packages/3/ms-3-gingo/main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"fmt"
-	"ms-3-gingo/src/environments"
-	"ms-3-gingo/src/routes"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	app := gin.New()
-	environments.InitEnv()
-	routes.InitRoutes(app)
-	fmt.Println(`ms-3 is runnig on: http://localhost:50003`)
-	app.Run("localhost:50003")
+	fmt.Print("ms-3-gingo")
 }
