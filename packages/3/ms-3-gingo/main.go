2ea3ab2729327ac00e096f233e6ab2cb7d4034ff
Gustavo Mahecha <gmahechas@ingenieros.com>
2022-08-04 22:42:51 -0500

    feat: vyper

--- packages/3/ms-3-gingo/main.go
+++ packages/3/ms-3-gingo/main.go
@@ -4,10 +4,12 @@ import (
 	"ms-3-gingo/routes"
 
 	ginGoUtils "github.com/gmahechas/erp-common-gin-go/utils"
+	msUtilsGoUtils "github.com/gmahechas/erp-common-ms-utils-go/utils"
 )
 
 func main() {
 	server := ginGoUtils.NewGinServer()
+	msUtilsGoUtils.InitEnv()
 	routes.InitRoutes(server)
 	server.Run(":50003")
 }
--- packages/common/erp-common-ms-utils-go/utils/env.go
+++ packages/common/erp-common-ms-utils-go/utils/env.go
@@ -1,7 +1,23 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type env struct {
+	environment string
+}
 
 func InitEnv() {
-	fmt.Println("InitEnv from Packages changes")
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+	vaultUrl, vaultRoleId, vaultSecretId := viper.GetString("VAULT_URL"), viper.GetString("VAULT_ROLE_ID"), viper.GetString("VAULT_SECRET_ID")
+	fmt.Println(vaultUrl)
+	fmt.Println(vaultRoleId)
+	fmt.Println(vaultSecretId)
 }
